21a9ec75f3dea758485a9a54afb9a0ee808340c5
znkevin0 <znkevin0@gmail.com>
2018-08-10 11:12:06 +0800

    更新可跟随系统改变app语言

--- app/src/main/java/com/example/junk/android_application_23/SettingActivity.java
+++ app/src/main/java/com/example/junk/android_application_23/SettingActivity.java
@@ -1,17 +1,27 @@
 package com.example.junk.android_application_23;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class SettingActivity extends AppCompatActivity {
+import com.example.junk.android_application_23.function.SetLanguageActivity;
 
+public class SettingActivity extends AppCompatActivity {
+    private Button mbtn_setting_language;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
-
+        mbtn_setting_language= (Button) findViewById(R.id.btn_open_setting);
+        mbtn_setting_language.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(SettingActivity.this, SetLanguageActivity.class);
+                startActivity(intent);
+            }
+        });
 
         /**
          * 返回
